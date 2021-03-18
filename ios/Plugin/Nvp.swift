13d4ac7a75e1ab584d6d80eb37215737edc038d1
snuffy <snuffy.tokyo@gmail.com>
2021-03-18 11:42:47 +0900

    fix: demo

--- ios/Plugin/Nvp.swift
+++ ios/Plugin/Nvp.swift
@@ -8,32 +8,36 @@ import PIPKit
 
     @objc public func start(_ playData: [[String:String]]) -> Void {
         
-        var playlist: [MediaItem] = [MediaItem(title:"hoge", album: "hoge", source: URL(string: "https://storage.googleapis.com/opcwc-295403.appspot.com/hls/M2I1MTM0MGQtMTYzZS00ZGZiLTljNjMtODRhYmFmN2E0MmU0/video.m3u8")!)]
-//        playData.forEach { (item) in
-//            guard let title = item["title"]?.removingPercentEncoding,
-//                let album = item["album"]?.removingPercentEncoding,
-//                let source = item["source"]?.removingPercentEncoding
-//            else {return}
-//
-//            // source が web なのか file なのかを判定する
-//            let regularURL = source.replacingOccurrences(of: "file://", with: "")
-//            var url: URL?
-//            //
-//            if regularURL.contains("http://") || regularURL.contains("https://") {
-//                url = URL(string: regularURL)
-//            }
-//            else {
-//                url = URL(fileURLWithPath: regularURL)
-//            }
-//            guard let sourceURL = url else {return}
-//
-//            let m = MediaItem(title: title, album: album, source: sourceURL)
-//            playlist.append(m)
-//        }
+        var playlist: [MediaItem] = []
+        
+        playData.forEach { (item) in
+            guard let title = item["title"]?.removingPercentEncoding,
+                let album = item["album"]?.removingPercentEncoding,
+                let source = item["source"]?.removingPercentEncoding
+            else {return}
+
+            // source が web なのか file なのかを判定する
+            let regularURL = source.replacingOccurrences(of: "file://", with: "")
+            var url: URL?
+            //
+            if regularURL.contains("http://") || regularURL.contains("https://") {
+                url = URL(string: regularURL)
+            }
+            else {
+                url = URL(fileURLWithPath: regularURL)
+            }
+            guard let sourceURL = url else {return}
+
+            let m = MediaItem(title: title, album: album, source: sourceURL)
+            playlist.append(m)
+        }
         if #available(iOS 11.0, *) {
-            let vc = CDVNativeVideoPlayerLayoutViewController()
-            vc.playlist = playlist
-            PIPKit.show(with: vc)
+            DispatchQueue.main.sync {
+                let vc = CDVNativeVideoPlayerLayoutViewController()
+                vc.playlist = playlist
+                PIPKit.show(with: vc)
+            }
         }
     }
 }
+    
